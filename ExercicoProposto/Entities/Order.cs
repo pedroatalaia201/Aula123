22048b2530c01d3110d8a4dedb33304b4da2f750
Pedro Lucas <pedro.lima3@estudante.ifms.edu.br>
2021-05-20 16:52:14 -0300

    continuando

---
+++ ExercicoProposto/Entities/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ExercicoProposto.Entities.Enums;
+
+namespace ExercicoProposto.Entities {
+    class Order {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Orders { get; set; } = new List<OrderItem>();
+
+        public void AddItem(OrderItem order) {
+            Orders.Add(order);
+        }
+
+        public void RemoveItem(OrderItem order) {
+            Orders.Remove(order);
+        }
+
+        public double Total() {
+            double total = 0;
+
+            foreach (var price in Orders) {
+                if(price.Price != 0.0) {
+                    total += price.Price;
+                }
+            }
+
+            return total;
+        }
+
+    }
+}
